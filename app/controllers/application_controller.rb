680c0256a855665f72df4b6109d674cb08ef5fab
Yeison <enmanuelquiterios@gmail.com>
2021-11-11 11:49:13 -0500

    Reviewing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
 
+
+
+  set :default_content_type, 'application/json'
   # add routes
+
+  get '/bakeries' do 
+    bakeries = Bakery.all 
+
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+
+    bakeries = Bakery.find(params[:id])
+    bakeries.to_json
+  end
+  
+  get '/baked_goods/by_price' do
+    price = BakedGood.all.order(:price)
+    binding.pry
+    price.to_json
+  end
 
 end
